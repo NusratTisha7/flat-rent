9eea0fc90cd114d0f0bcaf62cd8f17efcd6b4d54
Tisha <nusratjahantisha763@gmail.com>
2026-04-06 16:51:03 +0200

    host to vercel

--- Backend/middlewares/routes.js
+++
@@ -1,9 +0,0 @@
-const userRouter = require('../routers/userRouter');
-const propertyRouter = require('../routers/propertyRouter');
-const districtRouter =require('../routers/districtRouter')
-
-module.exports = (app) => {
-    app.use('/api/user', userRouter);
-    app.use('/api/property', propertyRouter);
-    app.use('/api/district',districtRouter)
-}
